5558b13885b7d7a3250750dc6956e5a4394f16f9
DESKTOP-5IBK0LR\22238 <2223852812@qq.com>
2023-12-25 22:06:50 +0800

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     dependencies{
-        classpath("com.android.tools.build:gradle:4.1.0")
         classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
-        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.0")
     }
 }
 plugins {
-    id("com.android.application") version "8.1.3" apply false
+    id("com.android.application") version "7.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.android.library") version "8.1.3" apply false
 }
